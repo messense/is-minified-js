c6aae77dc562aa05f3590374e2951fd22a7adb57
messense <messense@icloud.com>
2017-06-08 09:47:46 +0800

    Use ToPyErr trait from PyO3

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@ extern crate pyo3;
 extern crate might_be_minified;
 
 use std::fs;
-use pyo3::{PyResult, Python};
+use pyo3::{PyResult, Python, ToPyErr};
 use might_be_minified::analyze;
 
 
-fn is_likely_minified(_py: Python, path: &str) -> PyResult<bool> {
-    let mut f = fs::File::open(path)?;
+fn is_likely_minified(py: Python, path: &str) -> PyResult<bool> {
+    let mut f = fs::File::open(path).map_err(|e| e.to_pyerr(py))?;
     let res = analyze(&mut f);
     Ok(res.is_likely_minified())
 }
